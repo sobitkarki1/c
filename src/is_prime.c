8fa731b6249c6068000342bf1b372ace2942684c
S. Karki <sobitkarki7@gmail.com>
2024-08-16 21:45:49 +0545

    add checking prime program

---
+++ src/is_prime.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+bool isPrime(int num) {
+    if (num <= 1) return false;
+    if (num <= 3) return true;
+    if (num % 2 == 0 || num % 3 == 0) return false;
+
+    for (int i = 5; i * i <= num; i += 6) {
+        if (num % i == 0 || num % (i + 2) == 0) return false;
+    }
+
+    return true;
+}
+
+int main() {
+    int number;
+
+    // Asking the user to enter a number
+    printf("Enter an integer: ");
+    scanf("%d", &number);
+
+    // Checking if the number is prime
+    if (isPrime(number)) {
+        printf("%d is a prime number.\n", number);
+    } else {
+        printf("%d is not a prime number.\n", number);
+    }
+
+    return 0;
+}
